cffe5fca07132261fef4129196156f1f46b88440
paul <pgtech246@gmail.com>
2024-03-03 19:15:28 -0400

    Add lines to each page

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv('topics.csv')
 
+y_start = 21 # The starting y position for the lines
+
 for index, row in df.iterrows():
     pdf.add_page()
     # Set the header for the main pages
     pdf.set_font('Times', style="B", size=24)
     pdf.set_text_color(100, 100, 254)
     pdf.cell(w=0, h=16, txt=row['Topic'], align='L', ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+    
+    # Draw the lines
+    for y in range(21, 285, 10):
+        pdf.line(x1=10, y1=y, x2=200, y2=y)
 
     # Set the footer for main pages
     pdf.ln(260)
@@ -27,5 +32,9 @@ for index, row in df.iterrows():
         pdf.set_font('Times', style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+
+        # Draw the lines
+        for y in range(21, 285, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
 
 pdf.output('test.pdf')
